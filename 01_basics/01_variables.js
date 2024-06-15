95f5aaa12603ee0e395cea5499cbdb0e85da4b00
oyesheikh <oyeesheikh12@gmail.com>
2024-06-15 10:04:47 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144552
+let accountEmail = "faheemashraf997@gmail.com"
+var accountPassword = "faheem0011"
+accountCity = "Bahawalpur"
+let accountState;
+
+// accountId = 0099
+
+accountEmail = "hc@hc@gmail.com"
+
+
+accountPassword = "567"
+
+accountCity = "ctn" 
+
+console.log("accountEmail")
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState})
