db02720ba1d405b809f8b73928062d7b1df53219
oyesheikh <oyeesheikh12@gmail.com>
2024-06-15 10:30:39 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"Use strict"; //new version
+
+// alert(" 3 + 5 ") we are using node.js not browser
+
+console.log( 3 
+    + 3) //code readability high
+
+// console.log("Faheem")
+
+let Name = "Faheem"
+let age = 17
+// let is loggedin = false
+
+//number =>  2 to power 53
+//bigint
+//string=> ""
+//boolean => true/fale
+//null => stand alone value
+//undefined => no assigned the value
+//symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)//object
