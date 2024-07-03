ecd3d43158046e465c1630c8aba63359c2acc5d8
oyesheikh <oyeesheikh12@gmail.com>
2024-07-03 06:19:14 +0000

    conversionofdatatype

---
+++ 01_basics/03_conversion&operations.js
@@ -0,0 +1,35 @@
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// // console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+// convert number 33 easily convert but 33abc not convert give nan(not a number) value true is 1 and zero is 0
+
+// let isLoggedIn = 1 
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+// empty "" is false "faheem" is true
+// 1 = true
+
+
+let someNumbers = 33
+
+
+let stringNumbers = String(someNumbers)
+
+console.log(stringNumbers)
+console.log(typeof stringNumbers)
