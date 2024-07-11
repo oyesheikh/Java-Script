97c9fe2ea50e7457b2379180ed7804db94376dc9
oyesheikh <oyeesheikh12@gmail.com>
2024-07-11 05:55:44 +0000

    change

--- 01_basics/03_conversion&operations.js
+++ 01_basics/03_conversion&operations.js
@@ -26,10 +26,52 @@
 // 1 = true
 
 
-let someNumbers = 33
+// let someNumbers = 33
 
 
-let stringNumbers = String(someNumbers)
+// let stringNumbers = String(someNumbers)
 
-console.log(stringNumbers)
-console.log(typeof stringNumbers)
+// console.log(stringNumbers)
+// console.log(typeof stringNumbers)
+
+
+
+// ******** Operations ***********
+
+// let value = 3
+
+// let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+
+// let str2 = "hy"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+
+// console.log(1 + "2")
+
+// console.log("1" + 2 + 2)
+
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+
+// console.log(+"")
+
+let gameCounter = 1000
+gameCounter++
+
+console.log(gameCounter)
---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,40 @@
+// Primitive data types
+
+
+// 1st = String 
+// 2nd = number 
+// 3rd null 
+// 4th = boolean 
+// 5th = undefined 
+// 6th = symbol 
+// 7th =  big int  d
+
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime.20-Jun-2023
+
+const score = 100
+
+const isLoggedIn = false
+
+const bigNumber = 1284735
+
+// Reference data types / non-primitive
+
+// 1st = arrays 
+// 2nd = objects 
+// 3rd = functions 
+
+const heroes = [ "mighty raju" , "thor" , "supra man" ]
+
+let myObj = {
+       name: "Faheem",
+       age: 18,
+
+
+
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof bigNumber)
